b068f33710ef862e97f4df6599440a5ac3635ea2
EvilHui <Ukrumoski@gmail.com>
2025-05-07 16:44:22 +0500

    already complete first endpoint and handler for CreateOrder feature.

---
+++ src/Services/Catalog/Catalog.API/Products/CreateProduct/CreateProductHandler.cs
@@ -0,0 +1,38 @@
+﻿namespace Catalog.API.Products.CreateProduct;
+
+//Request Dto
+public record CreateProductCommand(
+    string Name,
+    List<string> Categories,
+    string Description,
+    string ImageFile,
+    decimal Price) : ICommand<CreateProductResult>;
+
+//Response Dto
+public record CreateProductResult(Guid Id);
+
+
+internal class CreateProductCommandHandler(IDocumentSession session)
+    : ICommandHandler<CreateProductCommand, CreateProductResult>
+{
+    public async Task<CreateProductResult> Handle(CreateProductCommand command, CancellationToken cancellationToken)
+    {
+        //create Product entity from command object
+        var product = new Product
+        {
+            Name = command.Name,
+            Categories = command.Categories,
+            Description = command.Description,
+            ImageFile = command.ImageFile,
+            Price = command.Price
+        };
+
+        //save to database
+        session.Store(product);
+        await session.SaveChangesAsync(cancellationToken);   
+        //return result (CreateProductResult)   
+
+        return new CreateProductResult(product.Id);
+    }
+}
+
